99e9f77a29320b9c63ea647b1d3bce615eee7d65
arunkumarjena <arunkumarjena18@gmail.com>
2021-06-15 10:42:11 +0530

    added multiply feature

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("Multiply " + (num1 * num2));
+    }   
+}
