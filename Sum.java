47d4d8bc510d52953ba58b1ed7e937c7c2c7d913
arunkumarjena <arunkumarjena18@gmail.com>
2021-06-15 10:30:38 +0530

    added function to Sum.java and changes in Diff.java

--- Sum.java
+++ Sum.java
@@ -1,8 +1,14 @@
 public class Sum{
+    public int add(int a, int b){
+        return a + b;
+    }
     public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
+        // int a = 5;
+        // int b = 6;
 
-        System.out.println("Sum = " + (a+b));
+        // System.out.println("Sum = " + (a+b));
+        // Creating object for it
+        Sum sum = new Sum();
+        System.out.println("Sum is : " + sum.add(49,10));
     }
 }
